2e2575f2a1b0f7b376dd0fc91bfee81807b48468
mohit shukla <mohit.shukla1992@gmail.com>
2026-02-28 13:22:51 +0530

    fix for monog connection error

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Use the PORT Render gives you, or default to 8080 for local testing
 const PORT = process.env.PORT || 8080;
 
-// Use the Cloud URI from Render, or fall back to your local DB
+// ALWAYS check process.env first! 
+// This is the line that fixed the "127.0.0.1" error.
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/shree_creations";
 
 module.exports = {
